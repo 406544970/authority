83e50b3177ab51f21cf0439d956ceb1b2ad4241c
flymanflyman <406544970@qq.com>
2019-04-22 14:37:30 +0800

    2019.4.22.4

--- src/main/java/com/lh/authority/controller/UniversalController.java
+++ src/main/java/com/lh/authority/controller/UniversalController.java
@@ -1,5 +1,9 @@
 package com.lh.authority.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -17,6 +21,7 @@ import static com.netflix.discovery.DiscoveryManager.*;
 @SpringBootApplication
 @EnableEurekaClient
 @RequestMapping(value = "/universal")     // 通过这里配置使下面的映射都在/users下，可去除
+@Api(value = "通用控制层", description = "专用于梁昊所要求的通用方法")
 public class UniversalController {
     public UniversalController() {
         super();
@@ -34,6 +39,7 @@ public class UniversalController {
     /**
      * @return 返回当前版本号
      */
+    @ApiOperation(value = "得到当前版本号", notes = "返回：当前版本号")
     @PostMapping("/myVersion")
     public String getVersion() {
         return String.format("I am is version:%s", version);
@@ -41,6 +47,7 @@ public class UniversalController {
     /**
      * @return 返回当前端口号
      */
+    @ApiOperation(value = "得到当前端口号", notes = "返回：当前端口号")
     @PostMapping("/myPort")
     public String myPort() {
         return String.format("I am is port:%s", port);
@@ -49,6 +56,7 @@ public class UniversalController {
     /**
      * @return 手动下线
      */
+    @ApiOperation(value = "手动下线方法", notes = "返回：当前微服务名和端口号")
     @GetMapping("/downLine")
     public String downLine() {
         getInstance().shutdownComponent();
