5916c4632d13615fb803b35128e3cecb1725fb4a
flymanflyman <406544970@qq.com>
2019-08-27 23:35:33 +0800

    2019.8.27.1

--- src/main/java/com/lh/authority/controller/UserController.java
+++ src/main/java/com/lh/authority/controller/UserController.java
@@ -48,22 +48,10 @@ public class UserController {
             @ApiImplicitParam(name = "pageNo", value = "当前页数", dataType = "int"),
             @ApiImplicitParam(name = "pageSize", value = "每页条数", dataType = "int")
     })
-//    @CrossOrigin(origins = "*")
     @PostMapping("/getUserList")
     public ResultVOPageTotal getUserList(@RequestParam(value = "id", required = false) String id
             , @RequestParam(value = "pageNo", defaultValue = "1") int pageNo
-            , @RequestParam(value = "pageSize", defaultValue = "10") int pageSize
-            , HttpServletResponse response, HttpServletRequest request) {
-        // 设置：Access-Control-Allow-Origin头，处理Session问题
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        System.out.println(request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("P3P", "CP=CAO PSA OUR");
-        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
-            response.addHeader("Access-Control-Allow-Methods", "POST,GET,TRACE,OPTIONS");
-            response.addHeader("Access-Control-Allow-Headers", "Content-Type,Origin,Accept");
-            response.addHeader("Access-Control-Max-Age", "120");
-        }
+            , @RequestParam(value = "pageSize", defaultValue = "10") int pageSize){
         PageHelper.startPage(pageNo, pageSize);
         List<User> userList = userService.getUserList(id);
         PageInfo pageInfo = new PageInfo<>(userList, pageSize);
