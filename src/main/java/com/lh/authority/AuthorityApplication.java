4ce6a5dbc8e5721ea3d8fd67e1975b58e4f4bc44
flymanflyman <406544970@qq.com>
2019-09-24 23:05:01 +0800

    2019.9.24.7

--- src/main/java/com/lh/authority/AuthorityApplication.java
+++ src/main/java/com/lh/authority/AuthorityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-@RefreshScope
 public class AuthorityApplication {
 
     public static void main(String[] args) {
